55a737c34036d1da4ac72627e9c721c6f889a20a
Ricky Tran <diabloxtran@gmail.com>
2022-10-15 13:56:25 -0400

    coded 7 CRUD actions and refactored using before_action, _form AND resources to replace routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,21 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  Rails.application.routes.draw do
+    get 'tasks', to: 'tasks#index'
+
+    get 'tasks/new', to: 'tasks#new', as: :new_task
+    post 'tasks', to: 'tasks#create'
+
+    # if task has dynamic portion to it, you need to pass it as an argument ex: "edit_task(@task)"
+    get 'tasks/:id', to: 'tasks#show', as: :task
+
+    get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+    patch 'tasks/:id', to: 'tasks#update'
+
+    delete 'tasks/:id', to: 'tasks#destroy'
+
+    resources :restaurants
+  end
 end
